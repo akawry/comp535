2fc3084e0c548009a7892e2bf2c678abef20b577
Alexander Kawrykow <akawry@cs.mcgill.ca>
2011-10-13 12:02:01 -0400

    removed grouter files. just keeping udp files

---
+++ udp.c
@@ -0,0 +1,9 @@
+/*
+ * udp.c (collection of functions that implement the UDP (User datagram protocol).
+ */
+
+#include "udp.h"
+#include "packetcore.h"
+#include <stdlib.h>
+#include <slack/err.h>
+#include <string.h>
---
+++ udp.h
@@ -0,0 +1,6 @@
+/*
+ * udp.h (collection of functions that implement the UDP (User datagram protocol).
+ */
+
+#ifndef __UDP_H_
+#define __UDP_H_
