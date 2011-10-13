a285fc3503a540d62dd78576718684241648c6d2
Alexander Kawrykow <akawry@cs.mcgill.ca>
2011-10-13 13:00:36 -0400

    compile script

---
+++ compile.sh
@@ -0,0 +1,6 @@
+export GINI_HOME=$HOME/gini
+export PATH=$PATH:$GINI_HOME/bin
+GINI_SRC=$GINI_HOME/gini-src/backend/
+cp udp.h $GINI_SRC/include/
+cp udp.c $GINI_SRC/src/grouter/
+cd $GINI_SRC && scons install
